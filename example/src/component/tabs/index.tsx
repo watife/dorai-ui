9150db135b70e7df34c58fed2e7354d385a5c071
Boluwatife Fakorede <watifefakorede@gmail.com>
2021-12-22 18:12:18 +0100

    feat(tabs-panel): add tabs panel and refactor tab call

--- example/src/component/tabs/index.tsx
+++ example/src/component/tabs/index.tsx
@@ -1,48 +1,33 @@
 import React from 'react'
 
-import { Tab } from 'dorai-ui'
+import { Tabs } from 'dorai-ui'
 
 function TabsComponent() {
   return (
-    <Tab.Group>
-      <Tab.List>
-        <Tab>
+    <Tabs>
+      <Tabs.List>
+        <Tabs.Trigger>
           {({ active }) => (
             <p style={{ border: active ? `1px solid red` : undefined }}>
               tab 1
             </p>
           )}
-        </Tab>
-        <Tab>
+        </Tabs.Trigger>
+        <Tabs.Trigger>
           {({ active }) => (
             <p style={{ border: active ? `1px solid red` : undefined }}>
               tab 2
             </p>
           )}
-        </Tab>
-        <Tab>
-          {({ active }) => (
-            <p style={{ border: active ? `1px solid red` : undefined }}>
-              tab 3
-            </p>
-          )}
-        </Tab>
-        <Tab disabled>
-          {({ active }) => (
-            <p style={{ border: active ? `1px solid red` : undefined }}>
-              tab 4
-            </p>
-          )}
-        </Tab>
-        <Tab>
-          {({ active }) => (
-            <p style={{ border: active ? `1px solid red` : undefined }}>
-              tab 5
-            </p>
-          )}
-        </Tab>
-      </Tab.List>
-    </Tab.Group>
+        </Tabs.Trigger>
+      </Tabs.List>
+      <Tabs.Panel>
+        <p>Panel Tab 1</p>
+      </Tabs.Panel>
+      <Tabs.Panel>
+        <p>Panel Tab 2</p>
+      </Tabs.Panel>
+    </Tabs>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 export * from './components/modal'
 export * from './components/switch'
 export * from './components/portal'
+export * from './components/tabs'
