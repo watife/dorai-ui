252debc1c749e8f121481c5656d01d458dd0f1b3
Boluwatife Fakorede <watifefakorede@gmail.com>
2021-12-29 00:32:12 +0100

    docs: add storybook for documentation

---
+++ packages/accordion/docs/accordion.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Accordion } from '../lib'
+
+export const Default: ComponentStory<typeof Accordion> =
+  function AccordionComponent() {
+    return (
+      <Accordion>
+        <Accordion.Group>
+          <Accordion.Header>
+            <Accordion.Trigger>open accordion</Accordion.Trigger>
+          </Accordion.Header>
+          <Accordion.Panel>This is the panel text</Accordion.Panel>
+        </Accordion.Group>
+        <Accordion.Group>
+          <Accordion.Header>
+            <Accordion.Trigger>open accordion 2</Accordion.Trigger>
+          </Accordion.Header>
+          <Accordion.Panel>This is the panel text 2</Accordion.Panel>
+        </Accordion.Group>
+      </Accordion>
+    )
+  }
+
+export default {
+  title: 'Accordion',
+  component: Accordion,
+  argTypes: {
+    vertical: {
+      default: true,
+      control: 'boolean'
+    },
+    defaultIndex: {
+      default: 0,
+      control: 'integer'
+    }
+  }
+} as ComponentMeta<typeof Accordion>
