54256340f903c4875cfbac7a1883164db72c5049
Boluwatife Fakorede <watifefakorede@gmail.com>
2022-01-13 08:34:24 +0100

    feat(alert-dialog): alert-dialog component

---
+++ packages/alert-dialog/__tests__/alert-dialog.test.tsx
@@ -0,0 +1,154 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { AlertDialog } from '../lib'
+
+type AlertDialogCompType = {
+  open?: boolean
+}
+
+const AlertDialogComp = ({ open }: AlertDialogCompType) => {
+  return (
+    <AlertDialog isOpen={open}>
+      <AlertDialog.Trigger>Open AlertDialog</AlertDialog.Trigger>
+      <AlertDialog.Group>
+        <AlertDialog.Title>Destructive activity</AlertDialog.Title>
+        <AlertDialog.Description>
+          This action cannot be reversed once clicked, please be certain you
+          want to proceed!
+        </AlertDialog.Description>
+        <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+        <AlertDialog.Action>Delete</AlertDialog.Action>
+      </AlertDialog.Group>
+    </AlertDialog>
+  )
+}
+
+describe('AlertDialog render', () => {
+  it("doesn't render if AlertDialog is not open", () => {
+    render(<AlertDialogComp />)
+
+    expect(screen.queryByRole(/alertdialog/i)).toBeNull()
+    expect(screen.queryByText(/destructive activity/i)).toBeNull()
+    expect(
+      screen.queryByText(
+        /This action cannot be reversed once clicked, please be certain you want to proceed!/i
+      )
+    ).toBeNull()
+  })
+
+  it('renders AlertDialog without crashing', () => {
+    render(<AlertDialogComp open />)
+
+    expect(screen.queryByRole(/alertdialog/i)).toBeInTheDocument()
+    expect(screen.getByText(/destructive activity/i)).toBeInTheDocument()
+    expect(
+      screen.getByText(
+        /This action cannot be reversed once clicked, please be certain you want to proceed!/i
+      )
+    ).toBeInTheDocument()
+  })
+
+  it('renders AlertDialog when AlertDialog Trigger is clicked', () => {
+    render(
+      <AlertDialog>
+        <AlertDialog.Trigger>Open AlertDialog</AlertDialog.Trigger>
+        <AlertDialog.Group>
+          <AlertDialog.Overlay />
+          <AlertDialog.Title>Title</AlertDialog.Title>
+          <AlertDialog.Description>
+            This action cannot be reversed once clicked, please be certain you
+            want to proceed!
+          </AlertDialog.Description>
+          <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+          <AlertDialog.Action>Action</AlertDialog.Action>
+        </AlertDialog.Group>
+      </AlertDialog>
+    )
+
+    const modalBtn = screen.getByText(/Open AlertDialog/i)
+
+    userEvent.click(modalBtn)
+
+    expect(screen.queryByRole(/alertdialog/i)).toBeInTheDocument()
+    expect(screen.getByText(/title/i)).toBeInTheDocument()
+    expect(
+      screen.getByText(
+        /This action cannot be reversed once clicked, please be certain you want to proceed!/i
+      )
+    ).toBeInTheDocument()
+    expect(screen.getByText(/cancel/i)).toBeInTheDocument()
+    expect(screen.getByText('Action')).toBeInTheDocument()
+  })
+
+  it('closes modal when Alert Dialog cancel is clicked', () => {
+    render(
+      <AlertDialog>
+        <AlertDialog.Trigger>Open AlertDialog</AlertDialog.Trigger>
+        <AlertDialog.Group>
+          <AlertDialog.Overlay />
+          <AlertDialog.Title>Title</AlertDialog.Title>
+          <AlertDialog.Description>
+            This action cannot be reversed once clicked, please be certain you
+            want to proceed!
+          </AlertDialog.Description>
+          <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+          <AlertDialog.Action>Delete</AlertDialog.Action>
+        </AlertDialog.Group>
+      </AlertDialog>
+    )
+
+    const modalBtn = screen.getByText(/Open AlertDialog/i)
+
+    userEvent.click(modalBtn)
+
+    userEvent.click(screen.getByText(/cancel/i))
+
+    expect(screen.queryByRole(/alertdialog/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/destructive activity/i)).not.toBeInTheDocument()
+    expect(
+      screen.queryByText(
+        /This action cannot be reversed once clicked, please be certain you want to proceed!/i
+      )
+    ).not.toBeInTheDocument()
+    expect(screen.queryByText(/cancel/i)).not.toBeInTheDocument()
+    expect(screen.queryByText('Action')).not.toBeInTheDocument()
+  })
+
+  it('performs expected action and closes dialog if action is clicked', () => {
+    const handleClick = jest.fn()
+    render(
+      <AlertDialog>
+        <AlertDialog.Trigger>Open AlertDialog</AlertDialog.Trigger>
+        <AlertDialog.Group>
+          <AlertDialog.Overlay />
+          <AlertDialog.Title>Title</AlertDialog.Title>
+          <AlertDialog.Description>
+            This action cannot be reversed once clicked, please be certain you
+            want to proceed!
+          </AlertDialog.Description>
+          <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+          <AlertDialog.Action onClick={handleClick}>Delete</AlertDialog.Action>
+        </AlertDialog.Group>
+      </AlertDialog>
+    )
+
+    const modalBtn = screen.getByText(/Open AlertDialog/i)
+
+    userEvent.click(modalBtn)
+
+    userEvent.click(screen.getByText(/delete/i))
+
+    expect(handleClick).toHaveBeenCalled()
+
+    expect(screen.queryByRole(/dialog/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/title/i)).not.toBeInTheDocument()
+    expect(
+      screen.queryByText(
+        /This action cannot be reversed once clicked, please be certain you want to proceed!/i
+      )
+    ).not.toBeInTheDocument()
+    expect(screen.queryByText(/cancel/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/action/i)).not.toBeInTheDocument()
+  })
+})
---
+++ packages/alert-dialog/lib/alert-dialog.tsx
@@ -0,0 +1,183 @@
+import React from 'react'
+import { Modal, useModalContext } from '@dorai-ui/modal'
+import { callAll, mergeRefs, Polymorphic } from '@dorai-ui/utils'
+
+/**
+ * Alert Dialog Context
+ */
+const AlertDialogContext = React.createContext<{
+  cancelRef: React.MutableRefObject<HTMLElement | null>
+} | null>(null)
+
+const useAlertDialogContext = (component: string) => {
+  const context = React.useContext(AlertDialogContext)
+
+  if (context === null) {
+    throw new Error(
+      `<AlertDialog.${component} /> component is not called within expected parent component`
+    )
+  }
+
+  return context
+}
+
+/**
+ *
+ *
+ * Root component
+ *
+ *
+ */
+
+type Arguments = {
+  isOpen: boolean
+}
+
+type ModalType = {
+  children: ((args: Arguments) => JSX.Element) | React.ReactNode
+  isOpen?: boolean
+  setIsOpen?: () => void
+  initialFocus?: React.MutableRefObject<HTMLElement | null>
+}
+
+const Root = ({ children, ...props }: ModalType) => {
+  const cancelRef = React.useRef<HTMLElement | null>(null)
+
+  const propsToPass = {
+    ...props,
+    initialFocus: props.initialFocus || cancelRef
+  }
+  return (
+    <AlertDialogContext.Provider value={{ cancelRef }}>
+      <Modal {...propsToPass}>{children}</Modal>
+    </AlertDialogContext.Provider>
+  )
+}
+
+/**
+ *
+ *
+ * Group component
+ *
+ *
+ */
+type GroupOwnProps = {
+  children: React.ReactNode
+}
+
+type GroupProps<C extends React.ElementType> =
+  Polymorphic.ComponentPropsWithRef<C, GroupOwnProps>
+
+const __DEFAULT_GROUP_TAG__ = 'div'
+
+type GroupType = <C extends React.ElementType = typeof __DEFAULT_GROUP_TAG__>(
+  props: GroupProps<C>
+) => React.ReactElement | null
+
+const Group: GroupType = React.forwardRef(
+  <C extends React.ElementType = typeof __DEFAULT_GROUP_TAG__>(
+    { as, children, ...props }: GroupProps<C>,
+    ref: Polymorphic.Ref<C>
+  ) => {
+    const TagName = as || __DEFAULT_GROUP_TAG__
+
+    return (
+      <Modal.Group as={TagName} role='alertdialog' {...props} ref={ref}>
+        {children}
+      </Modal.Group>
+    )
+  }
+)
+
+/**
+ *
+ *
+ * Cancel component which closes the Alert Dialog
+ *
+ *
+ */
+type CancelOwnProps = {
+  children: React.ReactNode
+}
+
+type CancelProps<C extends React.ElementType> =
+  Polymorphic.ComponentPropsWithRef<C, CancelOwnProps>
+
+const __DEFAULT_CANCEL_TAG__ = 'button'
+
+type CancelType = <C extends React.ElementType = typeof __DEFAULT_CANCEL_TAG__>(
+  props: CancelProps<C>
+) => React.ReactElement | null
+
+const Cancel: CancelType = React.forwardRef(
+  <C extends React.ElementType = typeof __DEFAULT_CANCEL_TAG__>(
+    { as, children, ...props }: CancelProps<C>,
+    ref: Polymorphic.Ref<C>
+  ) => {
+    const { cancelRef } = useAlertDialogContext('Cancel')
+
+    const mergedRef = mergeRefs([cancelRef, ref])
+
+    const TagName = as || __DEFAULT_CANCEL_TAG__
+
+    return (
+      <Modal.Close as={TagName} {...props} ref={mergedRef}>
+        {children}
+      </Modal.Close>
+    )
+  }
+)
+
+/**
+ *
+ *
+ * Action component
+ *
+ *
+ */
+type ActionOwnProps = {
+  children: React.ReactNode
+}
+
+type ActionProps<C extends React.ElementType> =
+  Polymorphic.ComponentPropsWithRef<C, ActionOwnProps>
+
+const __DEFAULT_ACTION_TAG__ = 'button'
+
+type ActionType = <C extends React.ElementType = typeof __DEFAULT_ACTION_TAG__>(
+  props: ActionProps<C>
+) => React.ReactElement | null
+
+const Action: ActionType = React.forwardRef(
+  <C extends React.ElementType = typeof __DEFAULT_ACTION_TAG__>(
+    { as, children, ...props }: ActionProps<C>,
+    ref: Polymorphic.Ref<C>
+  ) => {
+    const TagName = as || __DEFAULT_ACTION_TAG__
+
+    const { setIsOpen } = useModalContext('Action')
+
+    const handleClickEvent = callAll(props.onClick, () => setIsOpen(false))
+
+    const propsHandled = {
+      ...props,
+      onClick: handleClickEvent
+    }
+
+    return (
+      <Modal.Close as={TagName} {...propsHandled} ref={ref}>
+        {children}
+      </Modal.Close>
+    )
+  }
+)
+
+export const AlertDialog = Object.assign(Root, {
+  Group,
+  Cancel,
+  Action,
+  Trigger: Modal.Trigger,
+  Title: Modal.Title,
+  Description: Modal.Description,
+  Overlay: Modal.Overlay
+})
