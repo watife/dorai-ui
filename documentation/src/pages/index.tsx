e4d571721ee2cf3e4ee2f1c7f40eab91e8db990e
Boluwatife Fakorede <watifefakorede@gmail.com>
2022-02-04 22:36:10 +0100

    feat: documentation site

---
+++ documentation/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '@theme/Layout'
+import { HomePage } from './homepage'
+
+export default function Home(): JSX.Element {
+  return (
+    <Layout>
+      <HomePage />
+    </Layout>
+  )
+}
