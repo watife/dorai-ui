930bc1bcc3875fc86a08f05aed3c4c946dcd1fc1
Boluwatife Fakorede <watifefakorede@gmail.com>
2022-09-02 18:37:41 +0200

    fix: server side rendering hydration

--- packages/utils/src/get-id.ts
+++ packages/utils/src/get-id.ts
@@ -5,16 +5,42 @@ const generateId = () => {
   return ++id
 }
 
-const GetId = () => {
-  const [id, setId] = React.useState<number | null>(generateId)
+const GetId =
+  React.useId ??
+  function useId() {
+    const isServerReady = useServerSideReadiness()
+    const [id, setId] = React.useState<number | null>(
+      isServerReady ? generateId : null
+    )
+
+    React.useLayoutEffect(() => {
+      if (id) return
+      const generated = generateId()
+
+      setId(generated)
+    }, [id])
+
+    return id
+  }
+
+let isServerReady = false
+
+function useServerSideReadiness() {
+  const [serverIsReady, setServerIsReady] = React.useState(isServerReady)
+
   React.useEffect(() => {
-    if (id) return
-    const generated = generateId()
+    if (serverIsReady === true) return
 
-    setId(generated)
-  }, [id])
+    setServerIsReady(true)
+  }, [serverIsReady])
 
-  return id
+  React.useEffect(() => {
+    if (isServerReady === true) return
+
+    isServerReady = true
+  })
+
+  return serverIsReady
 }
 
 export { GetId, generateId }
