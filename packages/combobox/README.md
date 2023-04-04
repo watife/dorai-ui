0a1058d29fb8779cd948b39254b33308909e1956
Boluwatife Fakorede <watifefakorede@gmail.com>
2023-04-04 20:07:00 +0200

    feat: Dorai UI combobox

---
+++ packages/combobox/README.md
@@ -0,0 +1,11 @@
+# `@dorai-ui/combobox`
+
+> TODO: description
+
+## Usage
+
+```
+const combobox = require('@dorai-ui/combobox');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/combobox/docs/combobox.stories.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import { Combobox } from '../src'
+
+const DATA = [
+  { name: 'Addi', age: 10 },
+  { name: 'nftd', age: 11 },
+  { name: 'frt', age: 12 }
+]
+
+const filteredData = (search: string) =>
+  DATA.filter((item) => item.name.toLowerCase().includes(search))
+
+const Template: ComponentStory<typeof Combobox> = (args) => {
+  const [query, setQuery] = React.useState('nftd')
+
+  return (
+    <Combobox {...args} setValue={setQuery} value={DATA[0]}>
+      <Combobox.Input
+        as='input'
+        openOnFocus={true}
+        onChange={(e) => setQuery(e.target.value)}
+        defaultValue={query}
+      />
+      <Combobox.Options>
+        {filteredData(query).map((item) => (
+          <Combobox.Option
+            key={item.name}
+            value={item}
+            onClick={() => console.log(item.age)}
+          >
+            {({ active, selected }) => {
+              console.log(active, selected)
+              return <span>{item.name}</span>
+            }}
+          </Combobox.Option>
+        ))}
+      </Combobox.Options>
+    </Combobox>
+  )
+}
+
+export const Default = Template.bind({})
+Default.args = {}
+
+export default {
+  title: 'Combobox',
+  component: Combobox
+} as ComponentMeta<typeof Combobox>
